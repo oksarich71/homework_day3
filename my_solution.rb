06ab4021b5ca517770f474026022ab5c53139f6e
oksana richards <oksana_vldk@hotmail.com>
2018-09-12 16:47:47 +0100

    add station homework

---
+++ my_solution.rb
@@ -0,0 +1,14 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+stops.push ("Edinburgh Wavereley") #Add "Edinburgh Waverley" to the end of the array
+ stops.unshift ("Glasgow Queen St")#Add "Glasgow Queen St" to the start of the array
+stops.insert(3, "Polmont")
+#Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+ stops.index("Linlithgow")#Work out the index position of "Linlithgow"
+stops.delete("Livingston")#Remove "Livingston" from the array using its name
+stops.delete_at(1) #Delete "Cumbernauld" from the array by index
+
+ stops.length #How many stops there are in the array?
+
+p stops.select(1) #How many ways can we return "Falkirk High" from the array?
+#Reverse the positions of the stops in the array
+#Print out all the stops using a for loop
