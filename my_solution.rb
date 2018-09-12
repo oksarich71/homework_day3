ed4adaa6023ad8241c1bac92f255446c9fcf806d
oksana richards <oksana_vldk@hotmail.com>
2018-09-12 20:20:29 +0100

    Jonathan twitter

--- my_solution.rb
+++ my_solution.rb
@@ -13,7 +13,69 @@ stops[3]
  #How many ways can we return "Falkirk High" from the array?
  stops.reverse #Reverse the positions of the stops in the array
 
-for station in stops
-  p station
-end
+# for station in stops
+#   p station
+# end
 #Print out all the stops using a for loop
+
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+
+p users["Jonathan"][:twitter]
+# p users [:"Erik"] [:home_town]
+# p users [:"Erik"] [:lottery_numbers]
+# p users [:"Avril"][:pets][:species]
